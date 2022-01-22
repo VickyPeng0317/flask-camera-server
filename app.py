bee713a873b5d06b2cc621b20f2c56147353a534
SKYUSER-KUAA2RU\Administrator <cxz0917001997@gmail.com>
2022-01-22 15:29:27 +0800

    feat camera snapshot

--- app.py
+++ app.py
@@ -1,5 +1,18 @@
 from flask import Flask
+import cv2
+import base64
+
 app = Flask(__name__)
+
 @app.route("/")
 def hello():
     return "Hello, World!"
+
+@app.route("/camera")
+def camera():
+    cap = cv2.VideoCapture(0)
+    retval, image = cap.read()
+    retval, buffer = cv2.imencode('.jpg', image)
+    jpg_as_text = base64.b64encode(buffer)
+    cap.release()
+    return 'data: image/png;base64,' + jpg_as_text.decode("utf-8")
